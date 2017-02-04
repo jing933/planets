5d907c82b86534040c1654065ecaf330baaacdf1
jing933 <jing.wan933@gmail.com>
2017-02-04 16:47:01 -0500

    load the csv into sql database

--- analyze.py
+++ analyze.py
@@ -1,7 +1,18 @@
 import sqlite3
-connection = sqlite3.connect("survey.db")
+import csv
+connection = sqlite3.connect(":memory:")
 cursor = connection.cursor()
-cursor.execute("SELECT Site.lat, Site.long FROM Site;")
+cursor.execute("CREAT TABLE t(name,age);")
+
+with open('data.csv','r') as fin:
+  dr =csv.DictReader(fin)
+  to_db =[(i['name'],i['age']) for i  in dr]
+
+cursor.excutemany("INSERT INTO t (name,age) VALUES (?,?);", to_db)
+connection.commit()
+
+cursor=connection.cursor()
+cursor.execute("SELECT *FROM t;")
 results = cursor.fetchall()
 for r in results:
  print(r)
